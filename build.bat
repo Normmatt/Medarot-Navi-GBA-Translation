fdd63b8899d0bdf037240681a38b7fa578da3c43
Normmatt <normmatt234@gmail.com>
2015-07-27 19:46:23 +1200

    Fixed typo. Cleanup build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 copy rom\input.gba rom\output.gba
-##@armips.exe asm/SmallVWF.asm
 tools\grit\grit.exe ".\gfx\menu\007F14E8_eng.png" -gt -gB4 -m -mRtpf -mLf -ftb -fh! -o ".\gfx\menu\raw\007F14E8_eng"
 tools\GfxCompressor.exe ".\gfx\menu\raw\007F14E8_eng.img.bin" ".\asm\bin\gfx\007F14E8_eng.cmp.bin"
 copy .\gfx\menu\raw\007F14E8_eng.map.bin .\asm\bin\gfx\007F14E8_eng.map.bin
@@ -8,7 +7,7 @@ cd ./asm/c_replacements/
 call domake.bat
 cd ./../../
 armips.exe asm/Cheats.asm
-armips.exe asm/DrawCharacterC.asm -sym symfile.sym
+armips.exe asm/DrawCharacterC.asm
 armips.exe asm/gfx.asm
 armips.exe asm/script/script.asm
 pause
