ade5698b91ddd98ba1da2b6f2431002d5f0d05e5
unknown <edwinjimenezjr21@gmail.com>
2015-07-27 00:32:54 -0400

    Translated some menu text

--- asm/script/menu/strings.asm
+++ asm/script/menu/strings.asm
@@ -1,28 +1,36 @@
 ; Medarot Navi Pointers
 
 str_007EB9B8:
-	.string "Aボタンで 決定<NL>Bボタンで もどる<F3>"
+	;string "Aボタンで 決定<NL>Bボタンで もどる<F3>"
+	.string "<E4>Press A to examine<NL>Press B to cancel<F3>"
 
 str_007EB9CE:
-	.string "メダロットが1体も組まれていません!<F3>"
+	;string "メダロットが1体も組まれていません!<F3>"
+	.string "<E4>You don't have a single Medarot built!<F3>"
 
 str_005D5E9C:
-	.string "通信で対戦やパーツのこうかんをおこないます<NL>                        通信中<F3>"
+	;string "通信で対戦やパーツのこうかんをおこないます<NL>                        通信中<F3>"
+	.string "<E4>Link with friends to trade and battle.<NL>                        Connecting...<F3>"
 
 str_007EBA1C:
-	.string "セーブが 終わリました<F3>"
+	;string "セーブが 終わリました<F3>"
+	.string "<E4>Your progress has been saved.<F3>"
 
 str_005D5ED8:
-	.string "通信に失敗しました"
+	;string "通信に失敗しました"
+	string "<E4>Connection failed."
 
 str_005D5BB0:
-	.string "メダロッチを 起動します<F3>"
+	;string "メダロッチを 起動します<F3>"
+	.string "<E4>Boots up your Medarotch.<F3>"
 
 str_005D5B98:
-	.string "仲間を ブロックに 配置します<F3>"
+	;string "仲間を ブロックに 配置します<F3>"
+	.string "<E4>Place your Allies in a Block.<F3>"
 
 str_005D5B7C:
-	.string "ブロックで作られるメダロットの<NL>確認をします<F3>"
+	;string "ブロックで作られるメダロットの<NL>確認をします<F3>"
+	.string "<E4>Look at Medarots you<NL>created in a Block.<F3>"
 
 str_005D5B78:
 	.string "ダミー"
@@ -31,16 +39,19 @@ str_005D5B60:
 	.string "仲間の 能力を 確認します<F3>"
 
 str_005D5B4C:
-	.string "ゲームの 進行を ほぞんします<F3>"
+	;string "ゲームの 進行を ほぞんします<F3>"
+	.string "<E4>Record the game's progress.<F3>"
 
 str_005D5C0C:
-	.string "配置する ブロックを えらんでください<F3>"
+	;string "配置する ブロックを えらんでください<F3>"
+	.string "<E4>Select a Block.<F3>"
 
 str_005D5BF8:
 	.string "このブロックは<E8>を<NL>開発します<F3>"
 
 str_005D5BE4:
-	.string "このブロックには 配置できません<F3>"
+	;string "このブロックには 配置できません<F3>"
+	.string "<E4>You can't use this Block.<F3>"
 
 str_005D5BC0:
 	.string "配置する 仲間を えらんでください<NL>STARTで 決定<F3>"
@@ -52,22 +63,28 @@ str_005D5C58:
 	.string "通信対戦で 友達と<NL>きがるに ロボトルを 行います<F3>"
 
 str_005D5C3C:
-	.string "通信で 友達と パーツを こうかんします<F3>"
+	;string "通信で 友達と パーツを こうかんします<F3>"
+	.string "<E4>Trade parts with a friend.<F3>"
 
 str_005D5C24:
-	.string "通信で 友達に メダルを あげます<F3>"
+	;string "通信で 友達に メダルを あげます<F3>"
+	.string "<E4>Give a Medal to a friend.<F3>"
+
 
 str_005D5D4C:
 	.string "使用するメダロットを 決めてください<NL>STARTで 決定<F3>"
 
 str_005D5D30:
-	.string "2人まで えらんでください<NL>STARTで 決定<F3>"
+	;string "2人まで えらんでください<NL>STARTで 決定<F3>"
+	.string "<E4>Choose up to two people.<NL>Press START to start.<F3>"
 
 str_005D5D14:
-	.string "3人まで えらんでください<NL>STARTで 決定<F3>"
+	;string "3人まで えらんでください<NL>STARTで 決定<F3>"
+	.string "<E4>Choose up to three people.<NL>Press START to start.<F3>"
 
 str_005D5CF8:
-	.string "4人まで えらんでください<NL>STARTで 決定<F3>"
+	;string "4人まで えらんでください<NL>STARTで 決定<F3>"
+	.string "<E4>Choose up to four people.<NL>Press START to start.<F3>"
 
 str_005D5CD4:
 	.string "チームの メンバーを 決めてください<NL>STARTで 決定<F3>"
